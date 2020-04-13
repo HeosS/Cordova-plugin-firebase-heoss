4175d976a31e168f8e99b3e426736e172878b75a
HeosS <eoss13@gmail.com>
2020-04-13 19:52:06 -0400

    Update FirebasePluginInstanceIDService.java

--- src/android/FirebasePluginInstanceIDService.java
+++ src/android/FirebasePluginInstanceIDService.java
@@ -3,9 +3,9 @@ package org.apache.cordova.firebase;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class FirebasePluginInstanceIDService extends FirebaseMessagingService {
+public class FirebasePluginInstanceIDService extends FirebaseInstanceIdService {
 
     private static final String TAG = "FirebasePlugin";
 
@@ -14,7 +14,8 @@ public class FirebasePluginInstanceIDService extends FirebaseMessagingService {
      * the previous token had been compromised. Note that this is called when the InstanceID token
      * is initially generated so this is where you would retrieve the token.
      */
-    public void onNewToken() {
+    @Override
+    public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
